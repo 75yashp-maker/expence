74c024b79562f1b679ad3fdea5d2ba1980a63c28
75yashp-maker <75yashp@gmail.com>
2026-04-24 09:50:17 +0530

    Create expense.py

---
+++ expense.py
@@ -0,0 +1,10 @@
+class Expenses:
+    def __init__(self, title, amount, category, date):
+        self.title = title
+        self.amount = amount
+        self.category = category
+        self.date = date
+
+    def to_tuple(self):
+        return (self.title, self.amount, self.category, self.date)
+        
