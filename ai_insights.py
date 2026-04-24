6ad126be518d9de766c4b95de17952558c68c977
75yashp-maker <75yashp@gmail.com>
2026-04-24 10:03:26 +0530

    Create ai_insights.py

---
+++ ai_insights.py
@@ -0,0 +1,20 @@
+from groq import Groq
+client = Groq(api_key="#")
+
+def generate_insights(df):
+    summary= df.to_string()
+
+    prompt = f"""
+    analyze the following expense data and give insights:
+    {summary}
+
+    provide:
+    - spending patterns
+    - suggestions to save money
+    """
+
+    response = client.chat.completions.create(
+        model= "openai/gpt-oss-120b",
+        messages= [{"role":"user","content":prompt}]
+    )
+    return response.choices[0].message.content
